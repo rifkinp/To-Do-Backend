3f6ee84cd0eca46ab88937a097f2f9491fd131b1
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-07 14:40:46 +0800

    feat: add cors so it can be used as backend

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const userRoute = require('./user/userRoute');
 const todoRoute = require('./todo/todoRoute');
 
 const app = express();
 const PORT = 3000;
+
+app.use(
+  cors({
+    origin: '*',
+    credentials: true,
+  }),
+);
+
+app.options('*', cors());
+
 app.use(express.json());
 
 app.use('/user', userRoute);
