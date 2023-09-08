fb47d63aff888e2e7afe940bba9834678a9c1cdf
Rifki Nugraha <rifkichocobo@gmail.com>
2023-09-08 13:39:42 +0800

    test : fixing vercel error deploy with db

--- db/models/index.js
+++ db/models/index.js
@@ -1,19 +1,25 @@
-'use strict';
-
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
+
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 const config = require(__dirname + '/../config/database.json')[env];
 const db = {};
+const pg = require('pg')
 
 let sequelize;
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], {
+    ...config,
+    dialectModules: pg,
+  });
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, {
+    ...config,
+    dialectModules: pg,
+  });
 }
 
 fs
